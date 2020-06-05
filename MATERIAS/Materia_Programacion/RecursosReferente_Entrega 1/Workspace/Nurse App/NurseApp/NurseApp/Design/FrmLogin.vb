fb2a0f36d4f38fb1d8227e9f065a4d4b41c97fda
Agustín Yorel Acosta Bergonzoni <yorelacostabergonzoni@gmail.com>
2020-06-05 14:25:21 -0300

    Revision de datos

---
+++ MATERIAS/Materia_Programacion/RecursosReferente_Entrega 1/Workspace/Nurse App/NurseApp/NurseApp/Design/FrmLogin.vb
@@ -0,0 +1,5 @@
+﻿Public Class FrmLogin
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles ChkRememberUser.CheckedChanged
+
+    End Sub
+End Class
---
+++ MATERIAS/Materia_Programacion/RecursosReferente_Entrega 1/Workspace/Nurse App/PatientApp/PatientApp/Design/FrmSplashSreen.vb
@@ -0,0 +1,3 @@
+﻿Public Class FrmSplashSreen
+
+End Class
