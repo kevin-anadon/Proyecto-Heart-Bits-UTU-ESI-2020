7f682037cccc3b546d27b11a6235099d3937f6b0
YorelAcosta <yorelacostabergonzoni@gmail.com>
2020-06-20 20:41:36 -0300

    Solucionado: Archivo del DDL sometido a Testing

--- MATERIAS/Materia_BasesDatos/RecursosReferente_Entrega 1/Archivos/BDII_TelediagnosticoMedico - Heart Bits 3BB.sql
+++ MATERIAS/Materia_BasesDatos/RecursosReferente_Entrega 1/Archivos/BDII_TelediagnosticoMedico - Heart Bits 3BB.sql
@@ -12,18 +12,18 @@
 	-- ------------------------------------------------------------------------------ --
 	-- TABLAS DE CHAT --------------------------------------------------------------- --
 	CREATE TABLE mensaje (
-		id int (10) AUTO_INCREMENT NOT NULL,
-		texto varchar (MAX) NOT NULL,
-		hora date NOT NULL,
+		id INT (10) AUTO_INCREMENT NOT NULL,
+		texto BLOB (65535) NOT NULL,
+		hora TIME NOT NULL,
 		PRIMARY KEY (id)
 	);
 
 	CREATE TABLE salaChat (
-		id int (10) AUTO_INCREMENT NOT NULL,
-		estado boolean NOT NULL,
-		motivo varchar (128) NOT NULL,
-		fechaHoraInicio datetime NOT NULL,
-		fechaHoraFin datetime NOT NULL,
+		id INT (10) AUTO_INCREMENT NOT NULL,
+		estado BOOLEAN NOT NULL,
+		motivo VARCHAR (128) NOT NULL,
+		fechaHoraInicio DATETIME NOT NULL,
+		fechaHoraFin DATETIME NOT NULL,
 		PRIMARY KEY (id)
 	);	
 	-- ------------------------------------------------------------------------------ --
@@ -31,37 +31,37 @@
 
 	-- TABLAS ORIENTADAS HACIA LAS PERSONAS ----------------------------------------- --
 	CREATE TABLE paciente (
-		id int (10) AUTO_INCREMENT NOT NULL,
-		ci varchar (8) AUTO_INCREMENT NOT NULL,
-		primerNom varchar (32) NOT NULL,
-		segundoNom varchar (32),
-		primerApe varchar (32) NOT NULL,
-		segundoApe varchar (32) NOT NULL,
-		fechaNacimiento date NOT NULL,
-		email varchar (64) NOT NULL UNIQUE,
-		habilitado boolean NOT NULL,
+		id INT (10) AUTO_INCREMENT NOT NULL,
+		ci VARCHAR (8) NOT NULL UNIQUE,
+		primerNom VARCHAR (32) NOT NULL,
+		segundoNom VARCHAR (32),
+		primerApe VARCHAR (32) NOT NULL,
+		segundoApe VARCHAR (32) NOT NULL,
+		fechaNacimiento DATE NOT NULL,
+		email VARCHAR (64) NOT NULL UNIQUE,
+		habilitado BOOLEAN NOT NULL,
 		PRIMARY KEY (id)
 	);	
 
 	CREATE TABLE tipo_empleado (
-		id int (10) AUTO_INCREMENT NOT NULL,
-		nombre varchar (64) NOT NULL UNIQUE,
+		id INT (10) AUTO_INCREMENT NOT NULL,
+		nombre VARCHAR (64) NOT NULL UNIQUE,
 		PRIMARY KEY (id)
 	);	
 
 	CREATE TABLE empleado (
-		id int (10) AUTO_INCREMENT NOT NULL,
-		id_tipo int (10) NOT NULL,
-		ci varchar (8) AUTO_INCREMENT NOT NULL,
-		primerNom varchar (32) NOT NULL,
-		segundoNom varchar (32),
-		primerApe varchar (32) NOT NULL,
-		segundoApe varchar (32) NOT NULL,
-		fechaNacimiento date NOT NULL,
-		email varchar (64) NOT NULL UNIQUE,
-		usuario varchar (16) NOT NULL UNIQUE,
-		contrasena varchar (16) NOT NULL,
-		pin int (4) NOT NULL, 
+		id INT (10) AUTO_INCREMENT NOT NULL,
+		id_tipo INT (10) NOT NULL,
+		ci VARCHAR (8) NOT NULL UNIQUE,
+		primerNom VARCHAR (32) NOT NULL,
+		segundoNom VARCHAR (32),
+		primerApe VARCHAR (32) NOT NULL,
+		segundoApe VARCHAR (32) NOT NULL,
+		fechaNacimiento DATE NOT NULL,
+		email VARCHAR (64) NOT NULL UNIQUE,
+		usuario VARCHAR (16) NOT NULL UNIQUE,
+		contrasena VARCHAR (16) NOT NULL,
+		pin INT (4) NOT NULL, 
 		PRIMARY KEY (id),
 		FOREIGN KEY (id_tipo) REFERENCES tipo_empleado(id)
 	);	
@@ -70,64 +70,55 @@
 
 	-- TABLAS ORIENTADAS AL ÁREA MÉDICA --------------------------------------------- --
 	CREATE TABLE region (
-		id int (10) AUTO_INCREMENT NOT NULL,
-		nombre varchar (32) NOT NULL,
+		id INT (10) AUTO_INCREMENT NOT NULL,
+		nombre VARCHAR (32) NOT NULL,
 		PRIMARY KEY (id)
 	);	
 
 	CREATE TABLE zona (
-		id int (10) AUTO_INCREMENT NOT NULL,
-		id_region int (10) NOT NULL,
-		parte varchar (32) NOT NULL,
+		id INT (10) AUTO_INCREMENT NOT NULL,
+		id_region INT (10) NOT NULL,
+		parte VARCHAR (32) NOT NULL,
 		PRIMARY KEY (id),
 		FOREIGN KEY (id_region) REFERENCES region(id)
 	);	
 
 	CREATE TABLE prioridad (
-		id int (10) AUTO_INCREMENT NOT NULL,
-		nombre varchar (32) NOT NULL,
+		id INT (10) AUTO_INCREMENT NOT NULL,
+		nombre VARCHAR (32) NOT NULL,
 		PRIMARY KEY (id)
 	);	
 
 	CREATE TABLE sintoma (
-		id int (10) AUTO_INCREMENT NOT NULL,
-		id_zona int (10) NOT NULL,
-		descripcion varchar (128) NOT NULL,
+		id INT (10) AUTO_INCREMENT NOT NULL,
+		id_zona INT (10) NOT NULL,
+		descripcion VARCHAR (128) NOT NULL,
 		PRIMARY KEY (id),
 		FOREIGN KEY (id_zona) REFERENCES zona(id)
 	);	
 
 	CREATE TABLE patologia (
-		id int (10) AUTO_INCREMENT NOT NULL,
-		id_prioridad int (10) NOT NULL,
-		nombre varchar (32) NOT NULL,
-		descripcion varchar (128) NOT NULL,
-		indiceMortalidad int (10) NOT NULL,
+		id INT (10) AUTO_INCREMENT NOT NULL,
+		id_prioridad INT (10) NOT NULL,
+		nombre VARCHAR (32) NOT NULL,
+		descripcion VARCHAR (128) NOT NULL,
+		indiceMortalidad INT (10) NOT NULL,
 		PRIMARY KEY (id),
 		FOREIGN KEY (id_prioridad) REFERENCES prioridad(id)
 	);	
 
-	CREATE TABLE patologia (
-		id int (10) AUTO_INCREMENT NOT NULL,
-		id_prioridad int (10) NOT NULL,
-		nombre varchar (32) NOT NULL,
-		descripcion varchar (128) NOT NULL,
-		indiceMortalidad int (10) NOT NULL,
-		PRIMARY KEY (id),
-		FOREIGN KEY (id_prioridad) REFERENCES prioridad(id)
-	);	
 
 	CREATE TABLE tipo_diagnostico (
-		id int (10) AUTO_INCREMENT NOT NULL,
-		nombre varchar (64) NOT NULL UNIQUE,
+		id INT (10) AUTO_INCREMENT NOT NULL,
+		nombre VARCHAR (64) NOT NULL UNIQUE,
 		PRIMARY KEY (id)
 	);	
 
 	CREATE TABLE diagnostico (
-		id int (10) AUTO_INCREMENT NOT NULL,
-		id_tipo int (10) NOT NULL,
-		id_paciente int (10) NOT NULL,
-		id_sintoma int (10) NOT NULL,
+		id INT (10) AUTO_INCREMENT NOT NULL,
+		id_tipo INT (10) NOT NULL,
+		id_paciente INT (10) NOT NULL,
+		id_sintoma INT (10) NOT NULL,
 		PRIMARY KEY (id),
 		FOREIGN KEY (id_tipo) REFERENCES tipo_diagnostico(id),
 		FOREIGN KEY (id_paciente) REFERENCES paciente(id),
@@ -138,46 +129,46 @@
 
 	-- TABLAS ORIENTADAS A RELACIONES DE TABLAS ------------------------------------- --
 	CREATE TABLE paciente_sufre (
-		id_sintoma int (10) NOT NULL,
-		id_paciente int (10) NOT NULL,
+		id_sintoma INT (10) NOT NULL,
+		id_paciente INT (10) NOT NULL,
 		PRIMARY KEY (id_sintoma, id_paciente),
 		FOREIGN KEY (id_sintoma) REFERENCES sintoma(id),
 		FOREIGN KEY (id_paciente) REFERENCES paciente(id)
 	);
 
 	CREATE TABLE sintoma_compone (
-		id_sintoma int (10) NOT NULL,
-		id_patologia int (10) NOT NULL,
+		id_sintoma INT (10) NOT NULL,
+		id_patologia INT (10) NOT NULL,
 		PRIMARY KEY (id_sintoma, id_patologia),
 		FOREIGN KEY (id_sintoma) REFERENCES sintoma(id),
 		FOREIGN KEY (id_patologia) REFERENCES patologia(id)
 	);
 
 	CREATE TABLE peticion (
-		id_paciente int (10) NOT NULL,
-		id_medico int (10) NOT NULL,
-		estado boolean NOT NULL,
-		motivo varchar (128) NOT NULL,
-		fechaHoraInicio datetime NOT NULL,
-		fechaHoraFin datetime NOT NULL,
+		id_paciente INT (10) NOT NULL,
+		id_medico INT (10) NOT NULL,
+		estado BOOLEAN NOT NULL,
+		motivo VARCHAR (128) NOT NULL,
+		fechaHoraInicio DATETIME NOT NULL,
+		fechaHoraFin DATETIME NOT NULL,
 		PRIMARY KEY (id_paciente, id_medico),
 		FOREIGN KEY (id_paciente) REFERENCES paciente(id),
 		FOREIGN KEY (id_medico) REFERENCES empleado(id)
 	);
 
 	CREATE TABLE verifica (
-		id_tentativo int (10) NOT NULL,
-		id_medico int (10) NOT NULL,
+		id_tentativo INT (10) NOT NULL,
+		id_medico INT (10) NOT NULL,
 		PRIMARY KEY (id_tentativo),
 		FOREIGN KEY (id_tentativo) REFERENCES diagnostico(id),
 		FOREIGN KEY (id_medico) REFERENCES empleado(id)
 	);
 
 	CREATE TABLE conversa (
-		id_paciente int (10) NOT NULL,
-		id_medico int (10) NOT NULL,
-		id_mensaje int (10) NOT NULL,
-		id_sala int (10) NOT NULL,
+		id_paciente INT (10) NOT NULL,
+		id_medico INT (10) NOT NULL,
+		id_mensaje INT (10) NOT NULL,
+		id_sala INT (10) NOT NULL,
 		PRIMARY KEY (id_paciente, id_medico, id_mensaje),
 		FOREIGN KEY (id_paciente) REFERENCES paciente(id),
 		FOREIGN KEY (id_medico) REFERENCES empleado(id),
