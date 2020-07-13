f108eb1332af3955f2fbaf67ed1b875626c7b27f
Kevin Anadon <kevinxd77200@gmail.com>
2020-07-13 18:42:57 -0300

    Correciones de Scripts Entrega1, Credenciales del CentOS

---
+++ MATERIAS/Materia_SistemasOperativos/RecursosReferente_Entrega 2/Archivos/Scripts/ScriptsCorregidosEntrega1/backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Se hace un reload
+. $HOME/.bashrc
+
+#Exporto un respaldo de la base de datos, hacia el directorio Backup
+mysqldump -u root -pheartbits2002 --database Telediagnostico > /Telediagnostico/Backup/RespaldoTelediagnostico_v.$CANTBACKUP.sql
+
+#Aumento en 1 la variable de entorno CANTBACKUP
+Contador=(expr $CANTBACKUP + 1)
+echo "export CANTBACKUP=$Contador >> $HOME/.bashrc
+echo La base de datos ha sido respaldado con exito!!
+sleep 2 
+exit
