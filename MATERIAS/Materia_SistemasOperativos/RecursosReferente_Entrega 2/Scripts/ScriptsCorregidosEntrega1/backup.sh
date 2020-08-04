73a3063d39bd23cd137044fdf4ba822127a202b9
Kevin Anadon <mathew77200@hotmail.com>
2020-08-04 18:22:00 -0300

    Correcion de Scripts

--- MATERIAS/Materia_SistemasOperativos/RecursosReferente_Entrega 2/Scripts/ScriptsCorregidosEntrega1/backup.sh
+++ MATERIAS/Materia_SistemasOperativos/RecursosReferente_Entrega 2/Scripts/ScriptsCorregidosEntrega1/backup.sh
@@ -3,7 +3,7 @@
 . $HOME/.bashrc
 
 #Exporto un respaldo de la base de datos, hacia el directorio Backup
-mysqldump -u root -pheartbits2002 --database Telediagnostico > /Telediagnostico/Backup/RespaldoTelediagnostico_v.$CANTBACKUP.sql
+mysqldump -u root -pheartbits2002 --database TelediagnosticoMedico_HeartBits > /Telediagnostico/Backup/RespaldoTelediagnostico_v.$CANTBACKUP.sql
 
 #Aumento en 1 la variable de entorno CANTBACKUP
 Contador=(expr $CANTBACKUP + 1)
